4f846630572ceffed99ecdea46584a7d717a4e04
Rodrigo <rodrigo.custodio@yalo.com>
2026-04-13 17:20:24 -0600

    feat: Added buttons and cta messages
    - Adds support for CTA buttons.
    - Adds support for buttons messages.
    - Adds unit tests and integration tests for both

--- flutter-sdk/lib/src/domain/models/chat_message/cta_button.dart
+++ flutter-sdk/lib/src/domain/models/chat_message/cta_button.dart
@@ -1,13 +1,22 @@
 // Copyright (c) Yalochat, Inc. All rights reserved.
 
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'cta_button.g.dart';
 
 // A button with text and URL used in CTA messages
+@JsonSerializable()
 class CTAButton extends Equatable {
   final String text;
   final String url;
 
   const CTAButton({required this.text, required this.url});
+
+  factory CTAButton.fromJson(Map<String, dynamic> json) =>
+      _$CTAButtonFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CTAButtonToJson(this);
 
   @override
   List<Object?> get props => [text, url];
